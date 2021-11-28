6bf9d9efa674f5c38cf424621d89339f41d44feb
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-11-28 19:31:54 +0100

    Return sample list of values. Add delay

---
+++ app/src/main/java/es/voghdev/izvexample/IncidenceEntry.kt
@@ -0,0 +1,6 @@
+package es.voghdev.izvexample
+
+data class IncidenceEntry(
+    val title: String,
+    val incidenceLast14Days: Float
+)
--- app/src/main/java/es/voghdev/izvexample/IncidenceViewModel.kt
+++ app/src/main/java/es/voghdev/izvexample/IncidenceViewModel.kt
@@ -1,10 +1,12 @@
 package es.voghdev.izvexample
 
-class IncidenceViewModel : BaseViewModel() {
-    sealed class State: BaseViewModel.State() {
+class IncidenceViewModel(
+    val getIncidence: GetIncidenceUseCase
+) : BaseViewModel() {
+    sealed class State : BaseViewModel.State() {
         object Loading : State()
-        data class Error(val error: Throwable): State()
-        data class Loaded(val items: List<Item>): State()
+        data class Error(val error: Throwable) : State()
+        data class Loaded(val items: List<Item>) : State()
     }
 
     data class Item(
@@ -15,5 +17,24 @@ class IncidenceViewModel : BaseViewModel() {
         enum class Color {
             GREEN, YELLOW, ORANGE, RED
         }
+
+        companion object {
+            fun fromEntry(entry: IncidenceEntry) = Item(
+                title = entry.title,
+                incidenceLast14Days = entry.incidenceLast14Days,
+                color = when {
+                    entry.incidenceLast14Days < 100 -> Color.GREEN
+                    entry.incidenceLast14Days < 250 -> Color.YELLOW
+                    entry.incidenceLast14Days < 350 -> Color.ORANGE
+                    else -> Color.RED
+                }
+            )
+        }
+    }
+
+    suspend fun initialState() {
+        viewState.value = State.Loading
+        val entries = getIncidence()
+        viewState.value = State.Loaded(entries.map { Item.fromEntry(it) })
     }
 }
